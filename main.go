58c17f5818ea84d54aa3e72b557230796f8f84a0
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2024-09-08 01:22:01 +0300

    handle add books endpoint

--- main.go
+++ main.go
@@ -24,9 +24,47 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func addBook(c *gin.Context) {
+	// create a var of type book
+	// will be initialized with falsy values str => "", int => 0
+	var newBook book
+
+	// write the json body to the variable newBook
+	// this will not validate or check type of body extra or missing keys will not panic
+	// But BindJson will panic if the key exist with the wrong type
+
+	// If a field exists in the JSON Body, it will get parsed then update the newBook var with the value sent in the Json body
+	// Otherwise all other fields will not be changes
+	// extra fields will be discarded
+
+	// Note that you pass the memory address of the var so it can be updated by the BindJSON function
+	// if the type is wrong or any other error occur the err object will not be nil
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	// append a var to slice books
+	books = append(books, newBook)
+
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func getBook(c *gin.Context) {
+	// Get params from gin context
+	book, err := getBookById(c.Param("id"))
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
+	router.POST("/book", addBook)
 	router.Run("localhost:8080")
 }
